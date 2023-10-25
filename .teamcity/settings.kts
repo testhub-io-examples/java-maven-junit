13adce33b776f0ad5ea56359a6ff62ff22e38e2d
dkrupkina <dkrupkina@TeamCity>
2023-10-25 11:26:50 +0000

    TeamCity change in '[Copied Image] Java Maven JUnit + DSL' project: build step was copied in 'Build' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -85,6 +85,12 @@ object Build : BuildType({
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
+        maven {
+            name = "New build step"
+            id = "Maven"
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
     }
 
     triggers {
