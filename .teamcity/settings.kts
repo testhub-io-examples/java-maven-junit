35924f4baf398e65f1ce563541d4e0b4b4a6d096
dkrupkina <dkrupkina@TeamCity>
2023-10-26 11:24:17 +0000

    TeamCity change in 'Java Maven Junit' project: snapshot dependency was removed in 'Build' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -125,18 +125,6 @@ object Build : BuildType({
         perfmon {
         }
     }
-
-    dependencies {
-        snapshot(AbsoluteId("BookingApiPayconiq_Build")) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(AbsoluteId("JavaMavenDemo_Build")) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(AbsoluteId("SimpleMavenProjectWithTests_Build")) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-    }
 })
 
 object BuildFromMetaRunner : BuildType({
